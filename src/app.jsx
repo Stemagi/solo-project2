6f147695344a5fb3021cd0c3ce410f265796d229
stm <stm2485@gmail.com>
2023-03-16 15:56:14 +0200

    nav structure

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+}
---
+++ src/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export default function Header(){
+  <header className="header">
+    <nav className="header-nav">
+      <div className="header container">
+        <a href="" className="logo-btn"></a>
+          <a href="" className="header-btn">Каталог</a>
+          <a href="" className="header-btn">Горячее</a>
+          <a href="" className="header-btn">Поиск</a>
+      </div>
+    </nav>
+    <nav>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+    </nav>
+  </header>
+}
